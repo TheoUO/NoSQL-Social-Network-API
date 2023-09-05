bce78921314a8e400684def3cdba311cade67181
TCodes <theodoreuokeke@gmail.com>
2023-09-05 05:32:05 -0700

    fixed connection.js structure

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 // Importing the mongoose library
 const mongoose = require('mongoose');
-// Connecting to the MongoDB database using the MongoDB URI provided in the environment 
-// variables or using the default URI if the environment variable is not set
-mongoose.connect(process.env.MONOGDB_URI || 'mongodb://127.0.0.1:27017/social-network',{
+
+// Connecting to the database
+mongoose.connect(process.env.MONOGDB_URI || 'mongodb://127.0.0.1:27017/social-network',
+{
     useNewUrlParser: true, 
     useUnifiedTopology: true,
 });
 
 // Exporting the connection to the database as a module
-module.exports = mongoose.connection
+module.exports = mongoose.connection;
