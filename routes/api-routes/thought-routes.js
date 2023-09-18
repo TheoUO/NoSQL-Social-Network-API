4eeb4a1f26db1ff404beb9576b05a45465d4a084
TCodes <theodoreuokeke@gmail.com>
2023-09-17 17:35:12 -0700

    routes for thought

---
+++ routes/api-routes/thought-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    deleteThought,
+    updateThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thought-controller');
+
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought)
+    .post(addReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
